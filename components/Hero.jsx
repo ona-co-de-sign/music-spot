ddbc656a72571349fe2ec4ae950ea38705ee379f
Natalia <adelina09715@gmail.com>
2022-12-11 18:10:33 +0100

    change hero text

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,10 +4,10 @@ import imgHero from '@/img/hero.png'
 export default function Hero() {
     return (
         <div className='hero flex-row'>
-            <div className='flex-column'>
+            <div className='flex-column hero__content'>
                 <div className="hero__heading flex-column">
-                    <h1>discover, collect music and make a playlist on <span className='gradient-text'> musik </span> spot</h1>
-                    <p className='hero__text'>Entdecke Musikneuheiten sowie im Trend liegende Alben und erstelle deine eigene Playlist</p>
+                    <h1>discover, collect music and create your own playlist on <span className='gradient-text'> music </span> spot</h1>
+                    <p className='hero__text'>find out musical gems and explore new and trending artists and albums and create your own playlist</p>
                 </div>
                 <div className="hero__buttons flex-row">
                     <button className='btn-primary hero__btn'>entdecke</button>
