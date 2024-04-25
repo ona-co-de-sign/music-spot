4a8e3a9d7ebd3f723f9defaea999869894c3b917
Natalia <natalialexandra015@gmail.com>
2024-04-25 09:56:30 +0200

    add api for artists

---
+++ pages/api/artist-info.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+    const { artistName } = req.query;
+    try {
+        const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts|pageimages&exintro&explaintext&redirects=1&titles=${encodeURIComponent(artistName)}`);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching artist info:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
